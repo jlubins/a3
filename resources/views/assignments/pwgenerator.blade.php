62a043085c7290ecd80b681b3a31e2bb6d0531d9
John Lubinski <jwlubinski@gmail.com>
2017-04-10 14:01:26 -0400

    Form fix

--- resources/views/assignments/pwgenerator.blade.php
+++ resources/views/assignments/pwgenerator.blade.php
@@ -14,36 +14,33 @@
         <br>
         <form method='GET' action='/assignments/pwgenerator/generate'>
             <div class="row">
-                <div class="col-sm-4">
+                <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
                     <label for='text'>Select which base text you would like to use to generate your password.</label>
                     <div class="dropdown dropdown-dark">
                         <select name='text' id='text' class="dropdown-select">
-                <option value='choose'>Choose one...</option>
-                <option value='iliad.txt'>Homer's Iliad</option>
-                <option value='alice.txt'>Alice in Wonderland</option>
-                <option value='constitution.txt'>United States Constitution</option>
-              </select>
-                    </div>
-                </div>
-                <div class="col-sm-4">
-                    <label for='wordNumber'>Select the number of words you would like to be generated.</label>
-                    <div class="dropdown dropdown-dark">
-                        <select name="wordNumber" id="wordNumber" class="dropdown-select">
-                          <option value='3'>3</option>
-                          <option value='4'>4</option>
-                          <option value='5'>5</option>
-                          <option value='6'>6</option>
+                          <option value='choose'>Choose one...</option>
+                          <option value='iliad.txt'>Homer's Iliad</option>
+                          <option value='alice.txt'>Alice in Wonderland</option>
+                          <option value='constitution.txt'>United States Constitution</option>
                         </select>
                     </div>
                 </div>
+                <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                    <label for='wordNumber'>Select the number of words you would like to be generated.</label>
+                    <div class="inputGroupContainer">
+                      <div class="input-group">
+                        <textarea class="form-control" cols="5" rows="1" id="wordNumber" name="wordNumber"></textarea>
+                      </div>
+                    </div>
+                </div>
 
-                <div class="col-sm-2">
+                <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
                     <fieldset class='checkboxes'>
                         <label id="check1"><input type='checkbox' name='uppercaseCheckbox' value='isUppercase'>Uppercase</label><br><br>
                         <label id="check2"><input type='checkbox' name='inclNumberCheckbox' value='inclNumber'>Include Number</label><br><br>
                     </fieldset>
                 </div>
-                <div class="col-sm-2">
+                <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
                     <br>
                     <input type='submit' class='btn btn-primary btn-small'>
                 </div>
